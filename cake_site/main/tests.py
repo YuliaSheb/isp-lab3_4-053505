e5469221de773cda834cd5268601c0887a4f10e3
YuliaSheb <79199794+YuliaSheb@users.noreply.github.com>
2022-06-13 08:03:45 +0300

    Update tests.py

--- cake_site/main/tests.py
+++ cake_site/main/tests.py
@@ -6,30 +6,30 @@ from django.contrib.auth.models import User
 class TestAuthentification(TestCase):
 
     def test_registration(self):
-        form_data = {'username': "Nina", 'password1': "qwer05350113", 'password2': "qwer05350113"}
-        response = self.client.post("/Register", data=form_data)
+        form_data = {'username': "Yulia", 'password1': "12345", 'password2': "12345"}
+        response = self.client.post("/registration", data=form_data)
         self.assertEqual(response.status_code, 302)
 
     def test_login(self):
-        form_data = {'username': "Ngd", 'password': "qwer"}
+        form_data = {'username': "admiin", 'password': "12345"}
         response = self.client.post("/login", data=form_data)
         self.assertEqual(response.status_code, 200)
 
 class CakeCreateTestCase(TestCase):
     def setUp(self) -> None:
-        user = User.objects.create(username='Anna')
-        user.set_password('1234')
+        user = User.objects.create(username='Yulia')
+        user.set_password('12345')
         user.save()
-        self.client.login(username="Anna", password="1234")
+        self.client.login(username="Yulia", password="12345")
 
     def test_cake_post_valid_form(self):
-        form_data = {'name': "fish", "anons": "fish from russia",
-                     'price': "i don't know"}
+        form_data = {'name': "Красный бархат", "anons": "Супер вкусный",
+                     'price': "55"}
         response = self.client.post("/create", data=form_data)
         self.assertEqual(response.status_code, 200)
 
 
     def test_dcake_post_invalid_form(self):
-        form_data = {'name': "fish"}
+        form_data = {'name': "Красный бархат"}
         self.client.post("/create", data=form_data)
-        self.assertFalse(Cake.objects.filter(nameofdishes='fish').exists())
+        self.assertFalse(Cake.objects.filter(name='Красный бархат').exists())
